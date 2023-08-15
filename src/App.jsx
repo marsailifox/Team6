ba4f300b0bae2e875e61e5e4d198bd08f23f9445
EricHungGA <ehung303@gmail.com>
2023-08-15 11:23:23 -0700

    accessing user properties

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,19 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const { loginWithRedirect } = useAuth0()
-  const { logout } = useAuth0()
+  const { loginWithRedirect, logout, user } = useAuth0()
+  
+  console.log(user)
 
   return (
     <>
     {/* navbar goes here */}
     <button onClick={() => loginWithRedirect()}>Login / Sign up</button>
+    <button onClick={() => logout({ logoutParams: { returnTo: "http://localhost:5173" } })}>
+      Log Out
+    </button>
       <h1>Credit Rabbit</h1>
+      {user && <h2>Welcome {user.name}</h2>}
     </>
   )
 }
