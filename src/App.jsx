259d7cd5b3d7a7d398bc746647b517309dacaac7
EricHungGA <ehung303@gmail.com>
2023-08-15 11:13:44 -0700

    setting up react-oAuth

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { useAuth0 } from "@auth0/auth0-react"
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const { loginWithRedirect } = useAuth0()
+  const { logout } = useAuth0()
 
   return (
     <>
+    {/* navbar goes here */}
+    <button onClick={() => loginWithRedirect()}>Login / Sign up</button>
       <h1>Credit Rabbit</h1>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Auth0Provider } from '@auth0/auth0-react'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="credit-rabbit.us.auth0.com"
+    clientId="VIDXpjri41YwhXKONf5PvvKDhxplETau"
+    authorizationParams={{
+      redirect_uri: 'http://localhost:5173'
+    }}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>
 )
