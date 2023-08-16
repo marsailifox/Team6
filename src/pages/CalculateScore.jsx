f45841aa75ab035ce7e211460021610224cb83b6
markjt031 <markjt031@gmail.com>
2023-08-16 12:58:35 -0400

    form updated

--- src/pages/CalculateScore.jsx
+++ src/pages/CalculateScore.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import {useState} from 'react'
 
 function CalculateScore() {
+  const [formData, setFormData]=useState({})
+  
+  const calculate=(formData)=>{
+    console.log(formData)
+    console.log("Calculating")
+  }
+  const handleChange=(e)=>{
+    const name=e.target.name;
+    const value=e.target.value;
+    setFormData({...formData, [name] : value})
+  }
+  
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    calculate(formData)
+  }
   return <div>
     Calculate Your Score Page
+      <form onSubmit={handleSubmit}>
+        <input type='text' name='name' placeholder='Full Name' onChange={handleChange} />
+        <input type='text' name='age' placeholder='Age' onChange={handleChange}/>
+        <input type='text' name='loanAmount' placeholder='Loan Amount' onChange={handleChange}/>
+        <input type='text' name='employmentHistory' placeholder='Employment' onChange={handleChange}/>
+        <input type='text' name='income' placeholder='Income' onChange={handleChange}/>
+        <input type='text' name='creditHistoryLength' placeholder='Credit History Length' onChange={handleChange}/>
+        <input type='submit' value='Confirm'/>
+      </form>
     </div>;
 }
 
