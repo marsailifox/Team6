0c9b9acbc3d4d8bfd2fca76c9809929c5ecf45eb
EricHungGA <ehung303@gmail.com>
2023-08-16 11:55:16 -0700

    seeding csv data and adding raw data folder

--- models/example.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const exampleSchema = new Schema({
-    name: {type: String},
-    income: {type: Number},
-})
-
-module.exports = mongoose.model('Example', exampleSchema);
