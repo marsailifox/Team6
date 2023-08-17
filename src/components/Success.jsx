78981231af3bace14b770fda24159cb904cf2396
markjt031 <markjt031@gmail.com>
2023-08-17 13:27:50 -0400

    set up the result page

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Success = () => {
+const Success = ({loan}) => {
   return (
     <div>Success</div>
   )
--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import {useLocation} from 'react-router-dom'
+import Failure from '../components/Failure'
+import Success from '../components/Success'
 
 const Result = () => {
     const location=useLocation()
     console.log(location.state)
-    console.log("no")
+    
   return (
-    <div>Result</div>
+    <>
+        {location.state.loan.loan_status ? <Success loan={location.state.loan}/> : <Failure/>}
+    </>
   )
 }
 
