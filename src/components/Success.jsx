f3f089533a9350c5a139dcd905adcb1ee91f8e35
EricHungGA <ehung303@gmail.com>
2023-08-17 11:27:06 -0700

    starting styling of success page

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -4,11 +4,10 @@ import {Link} from 'react-router-dom'
 const Success = () => {
   return (
     <div>
-        <heading>
+        <img class="absolute top-0 left-0 mt-10 z-10 " style={{ height: '10rem', marginTop: '0.5rem', position: 'absolute', top:'0', left:'1rem' }} src="./Logo.jpg" alt="" />
             <h2>Congratulations!</h2>
             <h3>This Borrower's history is confirmed and qualified for the credit and loan.</h3>
             <p>Be ready for the next step with accessing the found</p>
-        </heading>
         <div>
             
         </div>
