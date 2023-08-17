220766f006fce75d5d06d1b495eeeb28c9bfe0f0
markjt031 <markjt031@gmail.com>
2023-08-17 11:41:19 -0400

    merging

---
+++ src/pages/Failure.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Failure = () => {
+  return (
+    <div>
+        <heading>
+            <h2>Something went wrong</h2>
+            <h3>This Borrower's history can't be qualified for the credit or loan.</h3>
+            <p>Please try again with different inputs</p>
+        </heading>
+        <a>Go back to the </a>
+    </div>
+  )
+}
+
+export default Failure
