5bfeb47c3caffe72b7c67350bd0ac640faead5e8
EricHungGA <ehung303@gmail.com>
2023-08-17 14:04:05 -0700

    changing uri / urls to updated deploy path

--- src/components/Failure.jsx
+++ src/components/Failure.jsx
@@ -10,7 +10,7 @@ const Failure = () => {
     <div className='flex'>
         <div class='ml-4'>
             <img src="Logo.jpg" width='100'/>
-            <button class='absolute top-0 right-0 mr-8 mt-4 rounded-lg bg-custom_orange w-32 h-10 text-white' onClick={() => logout({ logoutParams: { returnTo: "http://localhost:5173" } })}>Log Out</button>
+            <button class='absolute top-0 right-0 mr-8 mt-4 rounded-lg bg-custom_orange w-32 h-10 text-white' onClick={() => logout({ logoutParams: { returnTo: "http://credit-rabbit.vercel.app" } })}>Log Out</button>
             <div className='mt-8 ml-12 w-[525px]'> 
               <h2 className='text-3xl font-extrabold'>You may need to improve your credit.</h2>
               <h3 className='text-2xl font-bold mt-4'>Your history can't be qualified for the credit or loan.</h3>
