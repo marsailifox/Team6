1367c753912292de97375ff25b0916768f58a393
markjt031 <markjt031@gmail.com>
2023-08-17 13:45:10 -0400

    added go back link on fail page

--- src/components/Failure.jsx
+++ src/components/Failure.jsx
@@ -4,12 +4,12 @@ import {Link} from 'react-router-dom'
 const Failure = () => {
   return (
     <div>
-        <heading>
+        <div>
             <h2>Something went wrong!</h2>
             <h3>This Borrower's history can't be qualified for the credit or loan.</h3>
             <p>Please try again with different inputs</p>
-        </heading>
-        <Link to="#">Go back to the Borrower's look up page.</Link>
+        </div>
+        <Link to="/calculate">Go back to the Borrower's look up page.</Link>
     </div>
   )
 }
