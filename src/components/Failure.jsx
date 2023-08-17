5d390fc3ee82def4325cbc0057b9a344b3d6bf0d
markjt031 <markjt031@gmail.com>
2023-08-17 14:25:26 -0400

    failure page started

--- src/components/Failure.jsx
+++ src/components/Failure.jsx
@@ -3,13 +3,19 @@ import {Link} from 'react-router-dom'
 
 const Failure = () => {
   return (
-    <div>
+    <div className='flex'>
         <div>
-            <h2>Something went wrong!</h2>
-            <h3>This Borrower's history can't be qualified for the credit or loan.</h3>
-            <p>Please try again with different inputs</p>
-        </div>
-        <Link to="/calculate">Go back to the Borrower's look up page.</Link>
+            <img src="Logo.jpg" width='100'/>
+            <div className='mt-8 ml-12 w-[525px]'> 
+              <h2 className='text-3xl font-extrabold'>Something went wrong!</h2>
+              <h3 className='text-2xl font-bold'>This Borrower's history can't be qualified for the credit or loan.</h3>
+              <p className='mt-4 mb-10'>Please try again with different inputs</p>
+            </div>
+            <Link to="/calculate" className='ml-12 font-extrabold'>Go back to the Borrower's look up page.</Link>
+         </div>
+         <div className='translate-y-48 -translate-x-20 w-[400px]'>
+          <img src='Image used for Error Page.jpg' alt='failure image' width='400'></img>
+         </div>
     </div>
   )
 }
